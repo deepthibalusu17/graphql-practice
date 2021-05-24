bf4713f3e4c246bb5c3ba7e770414378d9993710
deepthibalusu17 <deepthibalusu@gmail.com>
2021-05-24 15:45:27 +0530

    first commit

---
+++ firstlettercapital.js
@@ -0,0 +1,8 @@
+// const str = 'captain picard';
+
+// function capitalize(str) {
+//   return str.charAt(0).toUpperCase() + str.slice(1);
+// }
+
+// const caps = str.split(' ').map(capitalize).join(' ');
+console.log("Hello World");
